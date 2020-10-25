f3ea421c148a1091df39b89f9f359189de356d74
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2020-10-25 13:57:32 +0530

    added new method to operators

--- operators.py
+++ operators.py
@@ -1,16 +1,22 @@
 """
 comparison operators
 
+ex: 1
 if temperature is greater than 30
     it's a hot day
 otherwise if it's less than 10
     it's a cold day
 otherwise
     it's neither hot nor cold
+
+ex. 2
+If name is less than 3 characters long -> name must be at least 3 characters
+Otherwise if it's more than 50 characters long -> name can be a maximum of 50 characters
+Otherwise -> name looks good !
 """
 
 
-def run_operators(temp):
+def run_operators_temp(temp):
     if temp > 30:
         print('it''s a hot day')
     elif temp < 10:
@@ -19,5 +25,15 @@ def run_operators(temp):
         print('it''s neither hot nor cold')
 
 
+def run_operators_name(name):
+    if len(name) < 3:
+        print('Name must be at least 3 characters')
+    elif len(name) > 50:
+        print('Name can be a maximum of 50 characters')
+    else:
+        print('Name looks good !')
+
+
 if __name__ == '__main__':
-    run_operators(30)
+    # run_operators_temp(30)
+    run_operators_name('Shehan Nandathilaka')
