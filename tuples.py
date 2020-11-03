5c88fdc8b294ebcf6fe0b6f90dd4cd08975dec5c
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2020-11-04 00:19:49 +0530

    unpacking for tuples and lists

--- tuples.py
+++ tuples.py
@@ -7,5 +7,32 @@ def execute_tuples():
     numbers = (1, 2, 3)
     print(numbers[0])
 
+
+def unpacking_tuple():
+    numbers = (4, 5, 6)
+
+    # this is same as following
+    # x = numbers[0]
+    # y = numbers[1]
+    # z = numbers[2]
+    x, y, z = numbers
+    print(f"{x}, {y}, {z}")
+    print(x * y * z)
+
+
+def unpacking_list():
+    numbers = [7, 8, 9]
+
+    # this is same as following
+    # x = numbers[0]
+    # y = numbers[1]
+    # z = numbers[2]
+    x, y, z = numbers
+    print(f"{x}, {y}, {z}")
+    print(x * y * z)
+
+
 if __name__ == '__main__':
-    execute_tuples()
+    # execute_tuples()
+    # unpacking_tuple()
+    unpacking_list()
