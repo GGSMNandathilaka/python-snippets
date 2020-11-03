0678841308eb47b2d5aa3508e8ca889ead66c5dc
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2020-11-03 23:26:03 +0530

    implemented a 2D list with a matrix

---
+++ matrix.py
@@ -0,0 +1,13 @@
+def execute_matrix():
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    for row in matrix:
+        for item in row:
+            print(item)
+
+if __name__ == '__main__':
+            execute_matrix()
