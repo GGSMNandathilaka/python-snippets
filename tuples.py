27d60615f4b89d5097f4bedd23a719d490e8f262
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2020-11-04 00:12:20 +0530

    created a tuple

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# we cannot modify tuples
+# tuples are immutable
+# for defining a tuple we will use paranthesis
+# we can use tuples in our applications as a static list (not changing)
+
+def execute_tuples():
+    numbers = (1, 2, 3)
+    print(numbers[0])
+
+if __name__ == '__main__':
+    execute_tuples()
