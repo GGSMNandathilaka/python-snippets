0441db019d0e00fe3e8fd5130f973241fba4b2fe
GGSMNandathilaka <ggsmnandathilaka@gmail.com>
2020-11-03 23:17:54 +0530

    implemented largest number finding logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 1
+num_arr = [12, 15, 2, 1, 7, 8, 23, 45, -1, -5]
 
-while i <= 5:
-    print('*' * i)
-    i += 1
-print('Done')
+largest_num = num_arr[0]
+for item in num_arr:
+    if item > largest_num:
+        largest_num = item
+print(f"Max: {largest_num}")
